8cd33fac11322e28a30dd624a06a8c7eeb055f25
Ben Wright <bwright@2u.com>
2023-09-15 14:05:00 -0500

    Adds drink tests

--- __tests__/drink.test.js
+++ __tests__/drink.test.js
@@ -1,3 +1,18 @@
-it("should run tests", () => {
-  expect(1 + 1).toBe(2);
+const Drink = require("../lib/drink.js");
+
+describe("Drink Class", () => {
+  describe("Instantiation", () => {
+    // it should be an instance drink object
+    it("should be an instance drink object", () => {
+      const drink = new Drink();
+      expect(drink).toBeInstanceOf(Drink);
+    });
+    // it should have a name, size & price in dollars
+    // it("should have a name, size & price in dollars", () => {
+    //   const drink = new Drink("Atomic Blackout", "Large", 5.75);
+    //   expect(drink.name).toBe("Atomic Blackout");
+    //   expect(drink.price).toBe(5.75);
+    //   expect(drink.size).toBe("Large");
+    // });
+  });
 });
